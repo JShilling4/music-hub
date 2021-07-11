96bf8e3c04837c2a8e36a387ed7d7b89571b9dd7
Justin Shilling <loveofmusic25@hotmail.com>
2021-07-10 23:14:35 -0400

    auth modal state in vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,20 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        authModalShow: false,
+    },
+    mutations: {
+        SHOW_AUTH_MODAL: (state, value) => {
+            state.authModalShow = value;
+        }
+    },
+    actions: {
+        openAuthModal: ({commit}) => {
+            commit("SHOW_AUTH_MODAL", true);
+        },
+        closeAuthModal: ({commit}) => {
+            commit("SHOW_AUTH_MODAL", false);
+        }
+    }
+});
