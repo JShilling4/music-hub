81fbd9545eb48bbc55e79158564e010fe1a76a36
Justin Shilling <loveofmusic25@hotmail.com>
2021-07-10 23:12:49 -0400

    breaking app up into components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,67 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+    <app-header />
+
+    <!-- Introduction -->
+    <section class="mb-8 py-20 text-white text-center relative">
+        <div
+            class="absolute inset-0 w-full h-full bg-contain introduction-bg"
+            style="background-image: url(assets/img/header.png)"
+        ></div>
+        <div class="container mx-auto">
+            <div class="text-white main-header-content">
+                <h1 class="font-bold text-5xl mb-5">
+                    Listen to Great Music!
+                </h1>
+                <p class="w-full md:w-8/12 mx-auto">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus et dolor
+                    mollis, congue augue non, venenatis elit. Nunc justo eros, suscipit ac
+                    aliquet imperdiet, venenatis et sapien. Duis sed magna pulvinar, fringilla
+                    lorem eget, ullamcorper urna.
+                </p>
+            </div>
+        </div>
+
+        <img
+            class="relative block mx-auto mt-5 -mb-20 w-auto max-w-full"
+            src="assets/img/introduction-music.png"
+        />
+    </section>
+
+    <!-- Main Content -->
+    <section class="container mx-auto">
+        <div class="bg-white rounded border border-gray-200 relative flex flex-col">
+            <div class="px-6 pt-6 pb-5 font-bold border-b border-gray-200">
+                <span class="card-title">Songs</span>
+                <!-- Icon -->
+                <i class="fa fa-headphones-alt float-right text-green-400 text-xl"></i>
+            </div>
+            <play-list />
+        </div>
+    </section>
+
+    <music-player />
+
+    <auth-modal v-show="authModalShow" />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { mapState } from "vuex";
 
-#nav {
-  padding: 30px;
-}
+import AppHeader from "@/components/AppHeader.vue";
+import AuthModal from "@/components/AuthModal.vue";
+import MusicPlayer from "@/components/MusicPlayer.vue";
+import PlayList from "@/components/PlayList.vue";
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default {
+    name: 'App',
+    components: {
+        "app-header": AppHeader,
+        "auth-modal": AuthModal,
+        "music-player": MusicPlayer,
+        "play-list": PlayList
+    },
+    computed: {
+        ...mapState(["authModalShow"])
+    }
 }
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
